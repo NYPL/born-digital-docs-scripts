b600cb9b14fab358462e7bce23bc449453ccae28
dreyjo <59994482+dreyjo@users.noreply.github.com>
2023-10-18 16:11:49 -0400

    changed test function name

--- noxfile.py
+++ noxfile.py
@@ -3,9 +3,9 @@ import nox
 versions = ["3.8", "3.9", "3.10", "3.11"]
 
 @nox.session(python=versions)
-def py_tests(session):
+def test(session):
     session.install(".")
-
+    
     for v in versions:
         session.run("pytest")
 
